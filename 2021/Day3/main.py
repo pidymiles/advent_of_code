8e553d12707f3c6083edc920b02358f92b84588e
pidymiles <miles.roat@gmail.com>
2021-12-03 20:01:17 -0500

    Update main.py

--- 2021/Day3/main.py
+++ 2021/Day3/main.py
@@ -1,21 +1,39 @@
+#Raw Binary Input with list comprehension
 binValues = []
-gammaRate = [0]*12
 
-with open("input.txt") as f:
+with open("./input.txt") as f:
     for line in f:
         binValues = [line.rstrip('\n') for line in f]
-        print(type(binValues[0]))
 
 
+##########
+# Part 1 #
+##########
+powerConsumtion = 0
+gammaRate = [0] * 12
+epsilonRate = [0] * 12
+
+#Loop over every string in binary values list
 for binValue in binValues:
-    for num in range(0,len(binValue)):
+    #Loop over every digit in each binary value
+    for num in range(0, len(binValue)):
         if binValue[num] == '0':
-           gammaRate[num] += -1
-       
-    
-        
+            gammaRate[num] -= 1
+            epsilonRate[num] -= 1
+        elif binValue[num] == '1':
+            gammaRate[num] += 1
+            epsilonRate[num] += 1
+            
+#Loop over every digit in each binary value
+for num in range(0, len(gammaRate)):
+    if gammaRate[num] < 0:
+        gammaRate[num] = 0
+        epsilonRate[num] = 1
+    elif gammaRate[num] > 0:
+        gammaRate[num] = 1
+        epsilonRate[num] = 0
 
-print(gammaRate)  
-    
-        
+print(gammaRate)
+print(epsilonRate)
+print("Power Consumption Rate: " + str(3069 * 1026))
 
